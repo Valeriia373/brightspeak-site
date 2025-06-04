fee8e22a028aa52624017c217c01e085406309e7
Valeriia373 <sverklera8606@gmail.com>
2025-06-04 14:24:35 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+﻿// Кнопка "Привітай мене!"
+function sayHello() {
+    alert("Привіт! Ласкаво просимо до BrightSpeak!");
+}
+
+// Кнопка "Натисни мене"
+function changeContent() {
+    const p = document.getElementById('welcome-text');
+    if (p.textContent === "А що тут в нас?") {
+        p.textContent = "Дякуємо за натискання!";
+    } else {
+        p.textContent = "А що тут в нас?";
+    }
+}
